d083d45a42775297c6a34987c18128823ecc5982
tianjunf <tianjunf@digitalchina.com>
2022-09-04 15:01:13 +0800

    add vue-router and pinia

--- billingFE_vite/src/main.js
+++ billingFE_vite/src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './index.css'
+import router from "./routers"
+
 const app = createApp(App)
+const pinia = createPinia()
+
 app.use(ElementPlus)
+app.use(router)
+app.use(pinia)
 
 app.mount('#app')
