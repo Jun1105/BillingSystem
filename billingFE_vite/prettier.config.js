42e8af310eb953b585048e49e84f0436ad6802a3
tianjun <tianjunf@digitalchina.com>
2022-09-05 15:41:22 +0800

    update prettier

--- billingFE_vite/prettier.config.js
+++ billingFE_vite/prettier.config.js
@@ -22,7 +22,7 @@ module.exports = {
   // 将 html 开始标签的后面一个 > 放在最后一行的末尾，而不是单独一行
   jsxBracketSameLine: false,
   // 当箭头函数只有一个参数时，是否加上括号
-  arrowParens: 'as-needed',
+  arrowParens: 'always',
   // 指定编译指示：只格式化在文件顶部包含特定注释的文件。
   requirePragma: false,
   // 文件已经被 Prettier 格式化之后，会在文件顶部插入一个特殊的 @format 标记。
