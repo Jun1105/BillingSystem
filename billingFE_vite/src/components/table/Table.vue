d1fcdf2ee56048ed3675b75107ddc343885521d3
tianjun <tianjunf@digitalchina.com>
2022-09-06 18:01:10 +0800

    封装table

---
+++ billingFE_vite/src/components/table/Table.vue
@@ -0,0 +1,46 @@
+<!-- eslint-disable @typescript-eslint/no-explicit-any -->
+<script setup lang="ts">
+  import { T } from 'unimport/dist/types-43c63a16'
+  import { ref } from 'vue'
+  interface Props {
+    tableData: Array<T>
+    tableColumn: Array<T>
+    total: number
+    handleSortChange?: any
+    handleCurrentChange?: any
+    handleSizeChange?: any
+  }
+  const props = defineProps<Props>()
+
+  const currentPage4 = ref(1)
+  const pageSize4 = ref(5)
+</script>
+<template>
+  <el-table
+    :data="props.tableData"
+    stripe
+    style="width: 100%"
+    max-height="800"
+    @sort-change="props.handleSortChange"
+  >
+    <template v-for="(item, index) in props.tableColumn" :key="index">
+      <el-table-column
+        :prop="item.prop"
+        :label="item.label"
+        :width="item.width"
+        :min-width="item.width"
+        :sortable="item.sort || false"
+        :formatter="item.formatter"
+      />
+    </template>
+  </el-table>
+  <el-pagination
+    v-model:currentPage="currentPage4"
+    v-model:page-size="pageSize4"
+    :page-sizes="[20, 40, 60, 100]"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="props.total"
+    @size-change="props.handleSizeChange"
+    @current-change="props.handleCurrentChange"
+  />
+</template>
--- billingFE_vite/src/view/order/Order.vue
+++ billingFE_vite/src/view/order/Order.vue
@@ -1,5 +1,132 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { reactive, ref } from 'vue'
+  import { Search } from '@element-plus/icons-vue'
+  import Table from '@/components/table'
+  const order = reactive({
+    type: null,
+    description: null,
+    amount: 0,
+    date: null
+  })
+  const typeList = ref([
+    {
+      label: '餐饮',
+      value: 1
+    }
+  ])
+
+  const tableData = ref([
+    {
+      date: '2016-05-03',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles'
+    },
+    {
+      date: '2016-05-02',
+      name: 'Tom',
+      address: 'No. 189, Grove St, Los Angeles'
+    }
+  ])
+  const tableColumn = ref([
+    {
+      prop: 'date',
+      label: 'Date'
+    },
+    {
+      prop: 'name',
+      label: 'Name'
+    },
+    {
+      prop: 'address',
+      label: 'Address'
+    }
+  ])
+
+  const disabledDate = (time: Date) => {
+    return time.getTime() > Date.now()
+  }
+
+  const searchForm = () => {
+    console.log(order)
+  }
+
+  const handleCurrentChange = (val: number) => {
+    console.log(`current page: ${val}`)
+  }
+  const handleSizeChange = (val: number) => {
+    console.log(`${val} items per page`)
+  }
+</script>
 <template>
-  <h1>账单</h1>
+  <el-card>
+    <el-form :model="order">
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-form-item label="类型：">
+            <el-select v-model="order.type" placeholder="please select type">
+              <el-option
+                v-for="item in typeList"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="描述：">
+            <el-input
+              type="text"
+              v-model="order.description"
+              placeholder="Such as breakfast"
+              maxlength="20"
+              show-word-limit
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="金额：">
+            <el-input-number
+              v-model="order.amount"
+              class="mx-4"
+              :min="0"
+              :precision="2"
+              controls-position="right"
+            />
+          </el-form-item>
+        </el-col>
+        <el-col :span="6">
+          <el-form-item label="时间：">
+            <el-date-picker
+              v-model="order.date"
+              type="date"
+              placeholder="Pick a day"
+              :disabled-date="disabledDate"
+            />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row justify="end">
+        <el-col :span="4">
+          <el-button type="success">
+            <el-icon><CirclePlus /></el-icon>
+            添加
+          </el-button>
+          <el-button type="primary" :icon="Search" @click="searchForm">
+            搜索
+          </el-button>
+        </el-col>
+      </el-row>
+    </el-form>
+  </el-card>
+  <el-card margin="1">
+    <Table
+      :table-data="tableData"
+      :table-column="tableColumn"
+      :total="5"
+      :handle-current-change="handleCurrentChange"
+      :handle-size-change="handleSizeChange"
+    ></Table>
+  </el-card>
 </template>
 <style lang="scss"></style>
