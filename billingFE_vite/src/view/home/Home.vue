a32e56fdd1142b8f8f4eeb6742009ce43ac65c51
tianjun <tianjunf@digitalchina.com>
2022-09-08 11:42:27 +0800

    home页布局

--- billingFE_vite/src/view/home/Home.vue
+++ billingFE_vite/src/view/home/Home.vue
@@ -2,6 +2,21 @@
 
 <script setup lang="ts"></script>
 <template>
-  <h1>Home</h1>
+  <el-row :gutter="20">
+    <el-col :xs="24" :sm="24" :md="12" :lg="12">
+      <el-card>
+        <h1 class="text_center">七日账单</h1>
+      </el-card>
+    </el-col>
+    <el-col :xs="24" :sm="24" :md="12" :lg="12">
+      <el-card>
+        <h1 class="text_center">周账单</h1>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
-<style lang="scss"></style>
+<style lang="scss">
+  .text_center {
+    text-align: center;
+  }
+</style>
