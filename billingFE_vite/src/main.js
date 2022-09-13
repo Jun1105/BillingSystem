b31c43f45286eefc37070b1c83ee7c5dd8766891
tianjun <tianjunf@digitalchina.com>
2022-09-13 15:35:00 +0800

    修改element ui plus语言版本

--- billingFE_vite/src/main.js
+++ billingFE_vite/src/main.js
@@ -10,12 +10,17 @@ import router from './routers'
 import i18n from './i18n'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import piniaPluginPersist from 'pinia-plugin-persist'
+import 'dayjs/locale/zh-cn'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersist)
 
-app.use(ElementPlus, { i18n: (key, value) => i18n.t(key, value) })
+app.use(ElementPlus, {
+  i18n: (key, value) => i18n.t(key, value),
+  locale: zhCn
+})
 app.use(router)
 app.use(pinia)
 app.use(i18n)
